eeaeecc278ad3cafe0d8747eede461eb123411bc
MaheshaBhat <maheshbhat2012@gmail.com>
2021-02-06 18:26:00 +0530

    add section header

---
+++ src/components/sectionHeader/index.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from "react";
+import './index.scss';
+
+interface Props {
+  title: string;
+  barColor: string;
+}
+
+const Header: FunctionComponent<Props> = ({ title, barColor }) => {
+  return (
+    <div className="header-container">
+      <div className="header-text">{title}</div>
+      <div className="bar" style={{ backgroundColor: barColor}}/>
+    </div>
+  );
+};
+
+export default Header;
