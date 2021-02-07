585bba1afb789b86340a88e56e8e405f9d7ba3be
MaheshaBhat <maheshbhat2012@gmail.com>
2021-02-07 13:35:44 +0530

    add bid amount

--- src/components/bidForm/index.tsx
+++ src/components/bidForm/index.tsx
@@ -1,10 +1,13 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 import "./index.scss";
 
 import SectionHeader from "../sectionHeader";
+import dollar from "../../assets/dollar.svg";
+
 
 const BidForm: FunctionComponent = () => {
+  const [amount, setAmount] = useState("");
 
   return (
     <div className="bid-form">
@@ -18,6 +21,31 @@ const BidForm: FunctionComponent = () => {
           marginTop: 0,
         }}
       />
+      <div
+        className="bid-amount-container"
+        style={{
+          justifyContent:
+            amount.toString().replace(",", "").length > 7 ? "left" : "center",
+        }}
+      >
+        <img src={dollar} />
+        <input
+          className="bid-amount"
+          value={amount}
+          placeholder="0"
+          pattern="[0-9]+([\,][0-9]+)?"
+          min="0"
+          max="5"
+          style={{
+            width: `${
+              amount != "" ? 32 * amount.toString().replace(",", "").length : 32
+            }px`,
+            opacity: amount != "" ? 1 : 0.12,
+          }}
+          type="number"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
