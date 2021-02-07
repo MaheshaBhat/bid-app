87651572ff68673f600fc61d78f76d11d03de239
MaheshaBhat <maheshbhat2012@gmail.com>
2021-02-07 14:55:41 +0530

    adjust layout margin and padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 
 import "./App.scss";
-import Header from './components/header';
+import Header from "./components/header";
 import SectionHeader from "./components/sectionHeader";
+import LayoutProvider from "./context";
+
+import { widthFactor, width } from "./constants";
+
+const offset: number = 0.05 * width * widthFactor;
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <SectionHeader title={"Old Car Available"} barColor={"#3054D6"} />
-    </div>
+    <LayoutProvider>
+      <div className="App">
+        <Header style={{ paddingLeft: offset, paddingRight: offset }} />
+        <SectionHeader title={"Old Car Available"} barColor={"#3054D6"} />
+      </div>
+    </LayoutProvider>
   );
 }
 
---
+++ src/constants/index.tsx
@@ -0,0 +1,4 @@
+export const widthFactor: number = window.screen.width / 375;
+export const width: number = window.screen.width;
+export const heightFactor: number = window.screen.height / 860;
+export const height: number = window.screen.height;
